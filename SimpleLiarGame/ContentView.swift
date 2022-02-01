72983f4960a69e92d8c0d7a405aa52eaded2a847
leewatermin <suminlee228@gmail.com>
2022-02-01 23:06:48 +0900

    Initial Commit

---
+++ SimpleLiarGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleLiarGame
+//
+//  Created by lee on 2022/02/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleLiarGame/SimpleLiarGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleLiarGameApp.swift
+//  SimpleLiarGame
+//
+//  Created by lee on 2022/02/01.
+//
+
+import SwiftUI
+
+@main
+struct SimpleLiarGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
